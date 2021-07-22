beb00d995e0ca70f6474e6c8fae8204136244070
cherylchao6 <nmpyt21@gmail.com>
2021-07-22 20:03:15 +0800

    handing heroid that does not exist

--- nodejsapp/server/models/hero_model.js
+++ nodejsapp/server/models/hero_model.js
@@ -135,7 +135,11 @@ const selectHeroes = async (requestDetail) => {
       return data[0][0];
     }
     if (heroId && member) {
-      let data = await pool.query(`SELECT heroes.hero_id as id, heroes.name, heroes.image, profile.str, profile.inte, profile.agi, profile.luk FROM heroes JOIN profile ON heroes.hero_id = profile.hero_id and heroes.date = profile.date WHERE heroes.date = '${lastestDate}' and heroes.hero_id = ${heroId}`);
+      let data = await pool.query(`SELECT heroes.hero_id as id, heroes.name, heroes.image, profile.str, profile.inte, profile.agi, profile.luk FROM heroes JOIN profile ON heroes.hero_id = profile.hero_id and heroes.date = profile.date WHERE heroes.date = '${lastestDate}' and heroes.hero_id = '${heroId}'`);
+      console.log(data[0]);
+      if (data[0].length === 0) {
+        return
+      }
       let profile = {
         str: data[0][0]['str'],
         int: data[0][0]['inte'],
--- nodejsapp/server/models/redis.js
+++ nodejsapp/server/models/redis.js
@@ -1,9 +1,9 @@
 const redis = require("redis");
-const client = redis.createClient({
-  port:6379,
-  host:'redis'
-});
-// const client = redis.createClient('6379');
+// const client = redis.createClient({
+//   port:6379,
+//   host:'redis'
+// });
+const client = redis.createClient('6379');
 
 
 function getCache (key) { // used in async function
